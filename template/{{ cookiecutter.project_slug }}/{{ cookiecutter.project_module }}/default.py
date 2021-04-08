b93c1868dfb297af5f1e18963ea06b66e6b2fe16
seldonplan <jonas0226@gmail.com>
2021-04-07 23:33:35 -0400

    mod default config, add default env

--- template/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_module }}/default.py
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_module }}/default.py
@@ -1,6 +1,13 @@
 # flake8: noqa
 # fmt:off
 
-# replace with application default configuration...
-# config object should be a standard python dict object with basic types, will be written out with toml writer
-DEFAULT_CONFIG = {"foo": "bar"}
+# replace with application configuration
+# should be a standard python dict object with basic types
+# will be parsed and written out with toml writer
+DEFAULT_CONFIG = {
+    "env": {
+        "default": {
+            "foo": "bar"
+        }
+    }
+}
