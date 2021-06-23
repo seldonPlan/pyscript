7e9bdfd109c4ff2924bae62dd4eefb2edebc70c5
seldonplan <jonas0226@gmail.com>
2021-06-23 13:49:36 -0400

    add default config, credentials load file to config module

---
+++ template/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_module }}/config/default.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+# fmt:off
+
+# replace with application configuration
+# should be a standard python dict object with basic types
+# will be parsed and written out using pytomlpp.dump
+DEFAULT_CONFIG = {
+    "foo": "bar"
+}
